88b2ab1d66d5dcd890526dec179721cc09a41b19
PlatformResources <143604028+PlatformResources@users.noreply.github.com>
2023-09-25 13:35:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region     = "us-east-1"
- }
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
 
 resource "aws_s3_bucket" "example" {
   bucket = "platform-resources-perficient-folks"
