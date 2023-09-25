e29a3d7149638798038489c4f91ad6bc073bc7f4
PlatformResources <143604028+PlatformResources@users.noreply.github.com>
2023-09-25 14:51:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the button element by its ID
+    var button = document.getElementById('myButton');
+    
+    // Add a click event listener to the button
+    button.addEventListener('click', function() {
+        alert('Button clicked!'); // Display an alert when the button is clicked
+    });
+});
