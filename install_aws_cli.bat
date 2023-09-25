affa76a036af78ca88c849632160d2b4a3ae0d8d
PlatformResources <143604028+PlatformResources@users.noreply.github.com>
2023-09-25 16:38:52 +0530

    Update install_aws_cli.bat

--- install_aws_cli.bat
+++ install_aws_cli.bat
@@ -2,7 +2,7 @@
 echo Installing AWS CLI v2...
 
 REM Download the AWS CLI v2 MSI installer
-curl -o AWSCLIV2.msi https://awscli.amazonaws.com/AWSCLIV2.msi
+curl.exe -o AWSCLIV2.msi https://awscli.amazonaws.com/AWSCLIV2.msi
 
 REM Install AWS CLI v2 silently
 msiexec.exe /i AWSCLIV2.msi /qn
@@ -11,3 +11,9 @@ REM Clean up the downloaded installer
 del AWSCLIV2.msi
 
 echo AWS CLI v2 installation complete.
+
+REM Extract terraform.rar using tar.exe (built-in Windows tool)
+echo Extracting terraform.rar...
+tar.exe -xf terraform.rar
+
+echo Terraform extraction complete.
